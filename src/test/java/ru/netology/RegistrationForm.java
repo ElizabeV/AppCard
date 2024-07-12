813f61e8db6fb964bbcdb1b821e8023c5c8a9fc2
Elizabeth Viricheva <liza.vir@ya.ru>
2024-07-12 12:29:40 +0300

    test added (ctrl c - exit from execution of jar)

---
+++ src/test/java/ru/netology/RegistrationForm.java
@@ -0,0 +1,39 @@
+package ru.netology;
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.*;
+
+class RegistrationForm {
+    @Test
+    void shouldSendFormWithLoad() {
+        open("http://localhost:9999");
+        $("[data-test-id='city'] input").setValue("Уфа");
+        //текущую дату ввести и прибавить 3
+        LocalDate currentDate = LocalDate.now().plusDays(3);
+        //отформатировать дату
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        String expectedDate = currentDate.format(formatter);
+        //дважды выделить и стереть
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        //найти поле
+        $("[data-test-id='date'] input").setValue(expectedDate);
+        $("[data-test-id='name'] input").setValue("Ваш-кин Павел");
+        $("[data-test-id='phone'] input").setValue("+78005553535");
+        $("[data-test-id='agreement']").click();
+        $("button.button").click();
+        $(".notification__content")
+                .shouldBe(visible, Duration.ofSeconds(15))
+                .shouldHave(exactText("Встреча успешно забронирована на " + expectedDate));
+    }
+}
